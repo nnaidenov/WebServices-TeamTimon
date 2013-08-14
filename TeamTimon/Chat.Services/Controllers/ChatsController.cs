8594410fd77148877f437153b83dc615e04864d7
Anton <ant.dobrev@gmail.com>
2013-08-14 05:57:18 -0700

    Controllers Added.

---
+++ TeamTimon/Chat.Services/Controllers/ChatsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Chat.Models;
+
+namespace Chat.Services.Controllers
+{
+    public class ChatsController : ApiController
+    {
+        private ChatEntities db = new ChatEntities();
+
+        // GET api/Chats
+        public IEnumerable<Chat.Models.Chat> GetChats()
+        {
+            return db.Chats.AsEnumerable();
+        }
+
+        // GET api/Chats/5
+        public Chat.Models.Chat GetChat(int id)
+        {
+            Chat.Models.Chat chat = db.Chats.Find(id);
+            if (chat == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return chat;
+        }
+
+        // PUT api/Chats/5
+        public HttpResponseMessage PutChat(int id, Chat.Models.Chat chat)
+        {
+            if (ModelState.IsValid && id == chat.ChatID)
+            {
+                db.Entry(chat).State = EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // POST api/Chats
+        public HttpResponseMessage PostChat(Chat.Models.Chat chat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Chats.Add(chat);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, chat);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = chat.ChatID }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // DELETE api/Chats/5
+        public HttpResponseMessage DeleteChat(int id)
+        {
+            Chat.Models.Chat chat = db.Chats.Find(id);
+            if (chat == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Chats.Remove(chat);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, chat);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TeamTimon/Chat.Services/Controllers/MessagesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Chat.Models;
+
+namespace Chat.Services.Controllers
+{
+    public class MessagesController : ApiController
+    {
+        private ChatEntities db = new ChatEntities();
+
+        // GET api/Messages
+        public IEnumerable<Message> GetMessages()
+        {
+            var messages = db.Messages.Include(m => m.Chat).Include(m => m.User);
+            return messages.AsEnumerable();
+        }
+
+        // GET api/Messages/5
+        public Message GetMessage(int id)
+        {
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return message;
+        }
+
+        // PUT api/Messages/5
+        public HttpResponseMessage PutMessage(int id, Message message)
+        {
+            if (ModelState.IsValid && id == message.MessageID)
+            {
+                db.Entry(message).State = EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // POST api/Messages
+        public HttpResponseMessage PostMessage(Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Messages.Add(message);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, message);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = message.MessageID }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // DELETE api/Messages/5
+        public HttpResponseMessage DeleteMessage(int id)
+        {
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Messages.Remove(message);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, message);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
